0b57b391ca40405244bf0fb72a84f667126a7c6b
Vincent Esposito <espov@pslogin02.slac.stanford.edu>
2021-02-24 11:15:25 -0800

    organize data files and gitignore

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ POLARITY = -1 # polarity of the waveform (positive or negative signal)
 SAVE_NUMBER = 200 # number of waveform to save when acquiring new set of reference
 COLORS = ['#ffa500', '#5d8aa8', '#800080', '#ecd540',  '#da70d6',
           '#87ceeb', '#fada5e', '#ff00ff', '#00ff00', '#d6cadd']
-TEST_DATA_FILE = './GEM_example_waveforms.csv'
+TEST_DATA_FILE = './refs/GEM_example_waveforms.csv'
